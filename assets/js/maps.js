9353de0f349cd86645a3efd210b98183d43325f1
Porsil <helenbrown13@hotmail.com>
2022-12-06 20:41:43 +0000

    add github search

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,12 +1,13 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 7,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 40.619261,
+            lng: -73.134766
         }
     });
 
+    // map markers
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var locations = [
         { lat: 40.785091, lng: -73.968285 },
